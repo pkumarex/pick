081a01fa0d06820504721b1677c1214372dc3839
pkumarex <PremalathaX.Kumareson@intel.com>
2022-09-27 18:15:14 +0530

    Delete vendor directory

--- vendor/golang.leonklingele.de/securetemp/securetemp_windows.go
+++
@@ -1,9 +0,0 @@
-package securetemp
-
-import (
-	"errors"
-)
-
-func createRAMDisk(size int) (string, func(), error) {
-	return mountPath, func() { return errors.New("windows is currently unsupported") }, nil
-}
